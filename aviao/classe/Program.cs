9d7e6db5b67c87b852b9e897e8358c7978b87960
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-02-28 12:37:06 -0300

    Add files via upload

---
+++ aviao/classe/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace aviao;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("AVIÃO");
+        Aviao aviao1 = new Aviao();
+        
+        aviao1.modelo = "Airbus A280";
+        aviao1.marca = "Azul";
+        
+        aviao1.exibir();
+        aviao1.acelerar();
+        aviao1.exibir();
+        aviao1.acelerar();
+        aviao1.exibir();
+    }
+}
